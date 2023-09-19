1fa4594b7eff2d8732ea23d4276c40c2a8aecc1d
Amir <106912785+am1r0d@users.noreply.github.com>
2023-09-19 17:17:24 +0330

    Create Splash Screen & Button component

---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+    green: "#129575",
+    white: "#FFFFFF",
+    black: "#000000",
+};
+
+export default colors;
---
+++ src/screens/Splash/Splash.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
+import styles from "./StylesSplash";
+import Button from "../../components/Button/Button";
+
+const Splash = () => {
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require("../../../assets/splash.png")}
+        >
+            {/* Icon + 100K+ */}
+            <View style={styles.container}>
+                <Image
+                    style={styles.logo}
+                    source={require("../../../assets/logo.png")}
+                />
+                <Text style={styles.title}>100K+ Premium Recipe</Text>
+            </View>
+
+            {/* Get cooking */}
+            <View style={styles.as}>
+                <Text style={styles.bigTitle}>Get</Text>
+                <Text style={styles.bigTitle}>Cooking</Text>
+                <Text style={styles.subtitle}>
+                    Simple way to find Tasty Recipe
+                </Text>
+
+                <Button>Start Cooking</Button>
+            </View>
+        </ImageBackground>
+    );
+};
+
+export default React.memo(Splash);
