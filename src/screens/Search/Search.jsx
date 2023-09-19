543a0fc1fdfce8e9af8eea1ed538c54ed77155e8
Amir <106912785+am1r0d@users.noreply.github.com>
2023-09-20 03:26:23 +0330

    Retrieving input value + implement search

--- src/screens/Search/Search.jsx
+++ src/screens/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { FlatList, SafeAreaView } from "react-native";
 import styles from "./StylesSearch";
 import Input from "../../components/Input/Input";
@@ -7,14 +7,28 @@ import Card from "../../components/Card/Card";
 
 const Search = () => {
     const { recipes } = useContext(RecipesContext);
-    const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [keyword, setKeyword] = useState("");
+
+    useEffect(() => {
+        if (keyword?.length > 2) {
+            const filteredItems = recipes?.filter((rec) =>
+                rec?.name?.toLowerCase()?.includes(keyword?.toLowerCase())
+            );
+            setFilteredRecipes(filteredItems);
+        } else {
+            setFilteredRecipes([]);
+        }
+    }, [keyword]);
 
     return (
         <SafeAreaView style={styles.container}>
-            <Input autoFocus />
+            <Input autoFocus onChangeText={setKeyword} value={keyword} />
+
             <FlatList
                 data={filteredRecipes}
                 keyExtractor={(item) => String(item?.id)}
+                style={{ flexGrow: 1 }}
                 numColumns={2}
                 renderItem={({ item, index }) => (
                     <Card
--- src/screens/Search/StylesSearch.jsx
+++ src/screens/Search/StylesSearch.jsx
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
-import colors from "../../constants/colors";
 
 const styles = StyleSheet.create({
-    container: { paddingHorizontal: 24 },
+    container: {
+        paddingHorizontal: 24,
+        flex: 1,
+    },
 });
+
 export default styles;
